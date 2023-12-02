8c0fa3971603cdd32f3c2612cedf3d81ecda65f8
abdur-tech <1622abdurrahmaan@gmail.com>
2023-12-02 12:37:03 +0530

    adding models

--- blog/views.py
+++ blog/views.py
@@ -1,51 +1,12 @@
 from datetime import date
-from django.shortcuts import render
-all_posts = [
-    {
-        "slug":"picture_1",
-        "image":"picture_1.jpg",
-        "author":"Abdur",
-        "date":date(2023,11,18),
-        "title":"title_picture_1",
-        "excerpt":"....Anything you want",
-        "content":".....content"
-    },
-    {
-        "slug":"picture_2",
-        "image":"picture_2.jpg",
-        "author":"Abdur",
-        "date":date(2023,11,18),
-        "title":"title_picture_2",
-        "excerpt":"....Anything you want",
-        "content":".....content"
-    },
-    {
-        "slug":"picture_3",
-        "image":"picture_3.jpg",
-        "author":"Abdur",
-        "date":date(2023,11,18),
-        "title":"title_picture_3",
-        "excerpt":"....Anything you want",
-        "content":".....content"
-    },
-    {
-        "slug":"picture_4",
-        "image":"picture_4.jpg",
-        "author":"Abdur",
-        "date":date(2023,11,18),
-        "title":"title_picture_4",
-        "excerpt":"....Anything you want",
-        "content":".....content"
-    }
-    
-]
+from django.shortcuts import render,get_object_or_404
+from .models import Post
 def get_date(post):
     return post['date']
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts=sorted(all_posts,key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(
         request,
         "blog/index.html",
@@ -55,6 +16,7 @@ def starting_page(request):
         )
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(
         request,
         "blog/all-posts.html",
@@ -64,7 +26,8 @@ def posts(request):
     )
 
 def post_detail(request,slug):
-    identified_post = next(post for post in all_posts if post["slug"]==slug)
+    identified_post = get_object_or_404(Post,slug=slug)
     return render(request,"blog/post-detail.html",
-                  {"post":identified_post}
-                  )
+                  {"post":identified_post,
+                  "posts_tags":identified_post.tags.all()
+                  })
